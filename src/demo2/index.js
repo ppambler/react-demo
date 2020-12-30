e9a6efbb09f67a42d59012c4325a87a2afa6afc4
Ambler <allinlbj@gmail.com>
2020-12-30 15:34:03 +0800

    让组件各自是独立的

--- src/demo2/index.js
+++ src/demo2/index.js
@@ -2,99 +2,123 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
-function App() {
-  return (
-    <div>
-      <Box1 name="+1" />
-      <Box2 name="+2" />
-    </div>
-  );
-}
 
 class App2 extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      number: 0
-    }
+      number: 0,
+    };
   }
   add() {
     this.setState({
-      number: this.state.number + 1
-    })
+      number: this.state.number + 1,
+    });
   }
   minus() {
     this.setState({
-      number: this.state.number - 1
-    })
+      number: this.state.number - 1,
+    });
   }
-  render() { // 局部 render
+  render() {
+    // 局部 render
     return (
       <div>
         App2 {this.props.name} {this.props.age}
         <div>
           <span className="tag">{this.state.number}</span>
-          <button className="button" onClick={this.add.bind(this)}>+1</button>
-          <button className="button" onClick={()=>this.minus()}> -1</button>
+          <button className="button" onClick={this.add.bind(this)}>
+            +1
+          </button>
+          <button className="button" onClick={() => this.minus()}>
+            {" "}
+            -1
+          </button>
         </div>
       </div>
-    )
+    );
   }
 }
-render();
+// render();
+// console.log(App())
 
-
-function Box1(obj) {
-  let number = 0;
-  let onClickButton = () => {
-    number += 1;
-    console.log(number)
-    render();
-  };
-  let onClickButton2 = () => {
-    number -= 1;
-    render();
-  };
-  return (
-    <div className="parent">
-      我的 name 是 {obj.name}
-      <span className="tag">{number}</span>
-      <button className="button is-primary" onClick={onClickButton}>
-        +
-      </button>
-      <button className="button is-primary" onClick={onClickButton2}>
-        -
-      </button>
-    </div>
-  );
+class Box1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0,
+    };
+  }
+  add() {
+    this.setState({
+      number: this.state.number + 1,
+    });
+  }
+  minus() {
+    this.setState({
+      number: this.state.number - 1,
+    });
+  }
+  render() {
+    return (
+      <div className="parent">
+        我的 name 是 {this.props.name}
+        <span className="tag">{this.state.number}</span>
+        <button className="button is-primary" onClick={this.add.bind(this)}>
+          +1
+        </button>
+        <button className="button is-primary" onClick={this.minus.bind(this)}>
+          -1
+        </button>
+      </div>
+    );
+  }
 }
-function Box2(obj) {
-  let number = 0;
-  let onClickButton = () => {
-    number += 2;
-    render();
-  };
-  let onClickButton2 = () => {
-    number -= 2;
-    render();
-  };
+class Box2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0,
+    };
+  }
+  add() {
+    this.setState({
+      number: this.state.number + 2,
+    });
+  }
+  minus() {
+    this.setState({
+      number: this.state.number - 2,
+    });
+  }
+  render() {
+    return (
+      <div className="parent">
+        我的 name 是 {this.props.name}
+        <span className="tag">{this.state.number}</span>
+        <button className="button is-primary" onClick={this.add.bind(this)}>
+          +2
+        </button>
+        <button className="button is-primary" onClick={this.minus.bind(this)}>
+          -2
+        </button>
+      </div>
+    );
+  }
+}
+
+function App(props) {
   return (
-    <div className="parent">
-      我的 name 是 {obj.name}
-      <span className="tag">{number}</span>
-      <button className="button is-primary" onClick={onClickButton}>
-        +
-      </button>
-      <button className="button is-primary" onClick={onClickButton2}>
-        -
-      </button>
+    <div>
+      <Box1 name="Box1" />
+      <Box2 name="Box2" />
     </div>
   );
 }
 
 function render() {
   ReactDOM.render(
-    <App2 name="LeBron" age={18}/>, // React.createElement(App)
+    <App />, // React.createElement(App)
     document.querySelector("#app")
   );
 }
@@ -107,4 +131,12 @@ function render() {
 //   );
 // }
 
-console.log(App());
+// console.log(App());
+
+render()
+
+let x = (<Box1 />)
+let y = (<Box1 />)
+console.log(x)
+console.log(y)
+console.log(x === y)
